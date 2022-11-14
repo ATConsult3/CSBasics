69fd322e68eef9ecf2e5e0e55926c663dd6a9288
Andrei <a0z9@rambler.ru>
2022-11-14 12:12:10 +0300

    Intro CS. Day1

---
+++ Day1_CS/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+using static System.Console;
+
+namespace andestech.learning.cs2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("HI all!");
+            //System.Console.WriteLine("HI all!");
+
+            WriteLine("HI all !!!");
+        }
+    }
+}
