c5d807e997c61e7eca35f990f368982689fc1b9e
Andrei <a0z9@rambler.ru>
2022-11-16 12:59:55 +0300

    Casting, Swap test, Day3

--- Day3_CS/AppClasses2/CastTest.cs
+++ Day3_CS/AppClasses2/CastTest.cs
@@ -4,6 +4,16 @@ namespace andestech.learning.cs2022
 {
     class CastTest
     {
+
+        public static void swap(ref int a, ref int b)
+        {
+            int c = a; a = b; b = c;
+        }
+        public static void swap(int a, int b)
+        {
+            int c = a; a = b; b = c;
+        }
+
         public static void Main() { 
         
             A a = new A { DataA = 1, Data=10 };
@@ -16,13 +26,32 @@ namespace andestech.learning.cs2022
             WriteLine("A.DataA=" + a2.DataA);
             WriteLine("A.DataA=" + a3.DataA);
 
-            //-----------------------
+            //-----------------------+
             WriteLine(" ------------------------ ");
            // A a21 = (A)o2;
             A a31 = o2 as A;
-           // WriteLine("A.DataA=" + a21.DataA);
-            WriteLine("A.DataA=" + a31?.Data);
+            if (o2.GetType() == typeof(B)) { WriteLine("B!!"); }
 
+            // WriteLine("A.DataA=" + a21.DataA);
+            int? data = a31?.Data ?? 0;
+            if (a31?.Data is null) { }
+
+            WriteLine("A.DataA=" + a31?.Data);
+            WriteLine("A.DataA=" + data);
+
+            // ----------- boxing|unboxing
+
+            int d1 = 100;
+            object o3 = d1;
+            //-----------
+            int d2 = (int)o3;
+
+            // swap test 
+            WriteLine(" --------------- SWAP TEST ------------");
+            int x = 111, y = 222;
+            WriteLine($"{x} -- {y}"); swap(ref x, ref y); WriteLine($"{x} -- {y}");
+            WriteLine(" --------------- TRYSWAP TEST ------------");
+            WriteLine($"{x} -- {y}"); swap(x, y); WriteLine($"{x} -- {y}");
 
         }
     }
