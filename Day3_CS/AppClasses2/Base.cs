1168ea1eb4cda628af1d79a839ee4cf0ffa2dc31
Andrei <a0z9@rambler.ru>
2022-11-16 12:35:06 +0300

    Casting, Day3

---
+++ Day3_CS/AppClasses2/Base.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace andestech.learning.cs2022
+{
+    internal class Base //: Object
+    {
+        public int Data { get; set; }
+
+    }
+
+    class A : Base {
+        public int DataA { get; set; }
+    }
+    class B : Base {
+        public int DataB { get; set; }
+    }
+
+}
---
+++ Day3_CS/AppClasses2/CastTest.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace andestech.learning.cs2022
+{
+    class CastTest
+    {
+        public static void Main() { 
+        
+            A a = new A { DataA = 1, Data=10 };
+            Base b = a;
+            object o = a;
+            object o2 = new B { Data=2, DataB=222};
+
+            A a2 = (A)o;
+            A a3 = o as A;
+            WriteLine("A.DataA=" + a2.DataA);
+            WriteLine("A.DataA=" + a3.DataA);
+
+            //-----------------------
+            WriteLine(" ------------------------ ");
+           // A a21 = (A)o2;
+            A a31 = o2 as A;
+           // WriteLine("A.DataA=" + a21.DataA);
+            WriteLine("A.DataA=" + a31?.Data);
+
+
+        }
+    }
+}
