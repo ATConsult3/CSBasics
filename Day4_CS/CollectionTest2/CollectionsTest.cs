c053a84a857dd0697d7e9e1e7cd40d85c4125447
Andrei <a0z9@rambler.ru>
2022-11-17 12:58:36 +0300

    Delegates, Lambdas - Day4

--- Day4_CS/CollectionTest2/CollectionsTest.cs
+++ Day4_CS/CollectionTest2/CollectionsTest.cs
@@ -25,15 +25,20 @@ namespace andestech.learning.cs2022
             echo("Linked LIst"); WriteLine();
             LinkedList<int> list = new LinkedList<int>( 
             new List<int>{1,2,-90,3,4,777,8,0,21});
+            
             echo(list);
 
             int[] arr = list.ToArray();
 
-            // sort linked list!
+            // sort linked list! long method
             List<int> list2 = new List<int>(list);
             list2.Sort();
             list = new LinkedList<int>(list2);
             echo(list);
+
+            // sort linked list! Extension OrderBy
+            var list3 =list.OrderBy(s => Math.Abs(s));
+            echo(list3);
 
             // stack
             echo("Stack"); WriteLine();
@@ -54,9 +59,31 @@ namespace andestech.learning.cs2022
             top = queue.Dequeue();
             echo(queue);
 
+            echo("Множества (Sets)"); WriteLine();
+            HashSet<int> set = new HashSet<int>();
+            set.Add(100);
+            set.Add(30);
+            set.Add(100);
+            set.Add(-10);
+            echo(set);
 
+            echo("Словари (Dictionanry)"); WriteLine();
+            Dictionary<String, int> greets = new Dictionary<String, int>
+            { {"Hi" , 0 },
+              {"Howdoyoudo", -2},
+              {"Салют", 20},
+              {"Hello", 5 }
+            };
 
+            greets["Hello"] = -50;
+            greets.Add("Good Morning", 2);
 
+            foreach (string key in greets.Keys) WriteLine($"{key} ---> {greets[key]}");
+            echo("Sorted Greets"); WriteLine();
+            var SortedGreets = greets.OrderBy(x => x.Key).Reverse().ToDictionary(x=> x.Key);
+            foreach (string key in SortedGreets.Keys) WriteLine($"{key} ---> {SortedGreets[key]}");
+
+         //   echo("Калькулятор"); WriteLine();
 
         }
     }
---
+++ Day4_CS/CollectionTest2/DelegateLabdaTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static andestech.learning.cs2022.utils.Utils;
+using System.Collections.ObjectModel;
+using static System.Console;
+
+namespace andestech.learning.cs2022
+{
+
+    public delegate void GreetsHandler(string name, int exclN=1);
+
+    internal class DelegateLabdaTest
+    {
+
+        private static void SayHi(string name, int N=1) => WriteLine($"Hi, {name}{new string('!', N)}");
+        private static void SayPrivet(string name, int N=1) => WriteLine($"Привет, {name}{new string('!', N)}");
+        private static void SayHDUD(string name, int N=1) => WriteLine($"Как жизнь, {name}?{new string('!', N)}");
+
+        private static void sayAll(Action<string, int> handler, string name, int N = 1)
+        { 
+         handler(name, N);
+         handler(name.ToUpper(), N);
+
+
+        }
+
+
+        public static void Main() {
+            echo("Delegate test");
+            GreetsHandler gh = SayHi;
+            gh("Andrei");
+            gh = SayPrivet;
+            gh("Andrei", 3);
+
+            gh += SayHDUD;
+            gh += SayHi;
+            gh += SayHi;
+            gh -= SayHi;
+
+            gh("Андрей", 3);
+
+            echo();
+
+            Action<string, int> greets2 =
+                (name, N) => WriteLine($"Салют, {name}{new string('!', N)}");
+
+            Action<string, int> gh2 = SayHDUD;
+            gh2 += SayHi;
+            gh2 += SayPrivet;
+            gh2 += (name, N) => WriteLine($"Добрый день, {name}{new string('!', N)}");
+            gh2 += greets2;
+            gh2("Андрей", 5);
+            echo("test delegate as argument");
+            sayAll(SayHDUD, "Andrei", 2);
+
+        }
+
+
+    }
+}
