47f29798278921815c52f6d4b03744d090d814ac
Andrei <a0z9@rambler.ru>
2022-11-17 17:10:14 +0300

    Reflection & FLAGS & Assembly - Day4

--- Day4_CS/CollectionTest2/Book.cs
+++ Day4_CS/CollectionTest2/Book.cs
@@ -13,6 +13,11 @@ namespace andestech.learning.cs2022.library
 {
     internal class Book
     {
+      private static Random r = new Random((int)DateTime.Now.Ticks);
+      private int _id = r.Next(0, 4_000_000_00);
+      public int ID { get => _id; }
+        
+
       public string Author { get; set; } 
       public string Title { get; set; }
         //.. todo
---
+++ Day4_CS/CollectionTest2/ReflectionTest.cs
@@ -0,0 +1,73 @@
+﻿#define LOGGER2
+#define LOGGER3
+#undef TRACE
+
+using System.Threading.Tasks;
+using static andestech.learning.cs2022.utils.Utils;
+using System.Collections.ObjectModel;
+using static System.Console;
+using andestech.learning.cs2022.library;
+using System.Diagnostics;
+using System.Runtime.Remoting.Messaging;
+using System.Reflection;
+using System;
+
+namespace andestech.learning.cs2022
+{
+    internal class ReflectionTest
+    {
+        public static void Main()
+        {
+            echo("Reflection & Flags test");
+
+            F1();
+            int i = 1;
+            Debug.Assert(i == 1, $"Debug: Hi, i={i}");
+            Trace.Assert(i == 3, $"Trace: Hi, i={i}");
+
+            Type type = typeof(Book);
+            PropertyInfo[] props = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            foreach (PropertyInfo prop in props) WriteLine(prop.Name + "---->" + prop.PropertyType +
+                " -->" + prop.Module);
+
+
+            Book book1 = new Book { Author = "А.Толстой", Title = "Аэлита" };
+            FieldInfo f1 = type.GetField("_id", BindingFlags.NonPublic | BindingFlags.Instance);
+            WriteLine("ID=" + book1.ID);
+            f1.SetValue(book1, 1);
+            WriteLine("ID=" + book1.ID);
+
+            // --- Lib loader test
+            echo("List of System.dll types...");
+            Assembly assembly = Assembly.LoadFrom(
+                "C:\\Program Files (x86)\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.8\\System.dll"
+                );
+            Type[] types = assembly.GetTypes();
+            foreach (Type t in types) WriteLine(t.FullName);   
+
+
+
+
+
+#if LOGGER3
+            F2();
+#endif
+#if DEBUG
+            WriteLine("DEBUG CODE TEST");
+#endif
+#if TRACE
+            WriteLine("TRACE CODE TEST");
+#endif
+
+        }
+
+        [Conditional("LOGGER1")]
+        [Conditional("LOGGER2")]
+        private static void F1() { WriteLine("+++ F1");}
+
+#if LOGGER3
+       private static int F2() { WriteLine("+++ F2!!!!"); return 0;}
+#endif
+
+    }
+}
