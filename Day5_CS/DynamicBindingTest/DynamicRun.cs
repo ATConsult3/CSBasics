488c4e4014036d007133779bd4d728b62bf9ce2d
Andrei <a0z9@rambler.ru>
2022-11-18 11:29:47 +0300

    Dynamic Library Loads v.2 - Day5

--- Day5_CS/DynamicBindingTest/DynamicRun.cs
+++ Day5_CS/DynamicBindingTest/DynamicRun.cs
@@ -12,6 +12,25 @@ namespace andestech.learning.cs2022
 {
     internal class DynamicRun
     {
+        private static Type Utype;
+
+        static DynamicRun() {
+            Assembly asm = Assembly.LoadFrom(
+                      "C:\\Users\\sand\\source\\repos\\csharp_basics_2\\Day4_CS\\Utils\\bin\\Debug\\Utils.dll"
+                      );
+            Utype = asm.GetType("andestech.learning.cs2022.utils.Utils");
+
+        }
+
+        static void Echo(string name="-", char symb='-', uint N=75) {
+            MethodInfo echo = Utype.GetMethod(
+                      name: "echo",
+                     types: new Type[] { typeof(string), typeof(char), typeof(uint) }
+                                 );
+            echo.Invoke(null, new object[] { name, symb, N });
+
+        }
+
         static void Main(string[] args)
         {
 
@@ -34,22 +53,24 @@ namespace andestech.learning.cs2022
             #endregion
 
             #region --- Выполнение кода экземпляра из динамически подкл. сборки -----
+           
+            Echo("Dynamic Lib Load");
+
             Assembly asm2 = Assembly.LoadFrom(
                     "C:\\Users\\sand\\source\\repos\\csharp_basics_2\\Day4_CS\\CollectionTest2\\" +
                     "bin\\Debug\\CollectionTest2.exe"
                     );
-            Type[] types = asm2.GetTypes();
-            foreach (Type t in types) WriteLine(t.FullName);
-
-            //Type Utype2 = asm2.GetType("andestech.learning.cs2022.utils.Utils");
-            //MethodInfo echo2 = Utype.GetMethod(
-            //   name: "echo",
-            //   //  bindingAttr: BindingFlags.Public | BindingFlags.Static,
-            //   types: new Type[] { typeof(string), typeof(char), typeof(uint) }
-            //    // ,modifiers : null, binder: null
-            //    );
-           
-
+            //Type[] types = asm2.GetTypes();
+            //foreach (Type t in types) WriteLine(t.FullName);
+            Type type = asm2.GetType("andestech.learning.cs2022.library.Book2");
+            
+            object book = Activator.CreateInstance(
+                  type: type,
+                  args: new object[] { "Артур Конан-Дойль", "Затерянный Мир" }
+                  );
+            WriteLine(book);
+            PropertyInfo id = type.GetProperty("ID", BindingFlags.Public | BindingFlags.Instance);
+            WriteLine("Book id: " + id.GetValue(book));
 
             #endregion
 
