6e2f9c65984d33b8ffad7a2d579f2b515bc2fa03
Andrei <a0z9@rambler.ru>
2022-11-18 11:51:02 +0300

    File Test - Day5

---
+++ Day5_CS/IOTest/IOTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using static System.Console;
+using System.Runtime.Remoting.Messaging;
+using System.Reflection;
+using static andestech.learning.cs2022.utils.Utils;
+using andestech.learning.cs2022.library;
+
+namespace andestech.learning.cs2022
+{
+    internal class IOTest
+    {
+        static void Main(string[] args)
+        {
+            echo("File test");
+        }
+    }
+}
