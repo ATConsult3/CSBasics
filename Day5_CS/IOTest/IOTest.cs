7d079e73571441c8980a294f9963d0e4d505cb52
Andrei <a0z9@rambler.ru>
2022-11-18 12:32:29 +0300

    File Test v.2 - Day5

--- Day5_CS/IOTest/IOTest.cs
+++ Day5_CS/IOTest/IOTest.cs
@@ -4,6 +4,9 @@ using System.Runtime.Remoting.Messaging;
 using System.Reflection;
 using static andestech.learning.cs2022.utils.Utils;
 using andestech.learning.cs2022.library;
+using System.IO;
+using System.Text;
+using System;
 
 namespace andestech.learning.cs2022
 {
@@ -11,7 +14,31 @@ namespace andestech.learning.cs2022
     {
         static void Main(string[] args)
         {
+            // File, FileInfo
+            
             echo("File test");
+            File.WriteAllText("file1.txt", "Text1..\nText2..\nМои данные\n", Encoding.UTF8);
+            string[] lines = File.ReadAllLines("file1.txt");
+            foreach (string line in lines) WriteLine(line);
+
+            FileInfo file1 = new FileInfo("file1.txt");
+            file1.CopyTo($"data_{DateTime.Now:yyyy_MM_dd_hh_mm_ss_fff}.log");
+            //file1.
+
+
+            // Directory, DirectoryInfo
+            Directory.CreateDirectory("dir2");
+            Directory.CreateDirectory("dir3\\dir4");
+
+            DirectoryInfo dir = new DirectoryInfo("dir1");
+            WriteLine(dir.Exists);
+
+
+
+
+
+
+
         }
     }
 }
